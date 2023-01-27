8cd9ad15a0a5d59618f4d446251941da18cdef42
Иван Гудырев <gie@EFTGroup.ru>
2023-01-27 17:59:37 +0300

    add tests of modals

---
+++ tests/test_buttons.py
@@ -0,0 +1,25 @@
+'''
+Buttons test
+'''
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+BTNS = [0, 1]
+URL = 'https://goodyrevqa.github.io/'
+
+@pytest.mark.parametrize('case', BTNS)
+def test_btns(browser, case):
+    """
+    Test of buttons
+    """
+    browser.get(URL)
+    WebDriverWait(browser, timeout=10, poll_frequency=0.5).until(EC.visibility_of_element_located(
+        (By.ID, "button2")))
+
+    cards = browser.find_elements(By.CSS_SELECTOR, value='[class="btn"]')
+    cards[case].click()
+
+    modal = browser.find_element(By.CSS_SELECTOR, value='[class="swal-title"]')
+    assert modal.text in ["Умею:", "FAC SI FACIS"]
